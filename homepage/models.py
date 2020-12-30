8b8796dfcd23b2ef9b904d138cf11e50ad87d1e9
Vilhelm <34233414+Vilperi27@users.noreply.github.com>
2020-12-30 11:05:52 +0200

    Search functionality added

--- homepage/models.py
+++ homepage/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Tuote(models.Model):
+    product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=150)
     product_description = models.CharField(max_length=1000)
     product_image = models.CharField(max_length=2000)
     product_price = models.DecimalField(max_digits=65, decimal_places=2, default=0)
+
+    
--- homepage/views.py
+++ homepage/views.py
@@ -4,6 +4,12 @@ from homepage.models import Tuote
 # Create your views here.
 
 def home(request):
+
+    if request.method == "POST":
+        term = request.POST.get('search')
+        tuotteet = Tuote.objects.filter(product_name__contains=term) | Tuote.objects.filter(product_id__contains=term)
+        return render(request, 'home.html', {'product':tuotteet})
+
     tuotteet = Tuote.objects.all()
     return render(request, 'home.html', {'product':tuotteet})
 
